8d0b6e478b86b52649bbfad646ca8ddadf257d36
LehmanBrother <chrislehman1991@gmail.com>
2018-09-25 09:32:41 -0500

    made initial file structure and empty tamagotchi class

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//Make Tamagotchi class
+
+class Tamagotchi {
+	constructor() {
+
+	}
+}
+
+//Instantiate
+
+//Display image to represent pet
+
+//Display metrics:
+	//Hunger (1-10)
+	//Sleepiness (1-10)
+	//Boredom (1-10)
+	//Age
+
+//Buttons to feed, turn off lights, play
+
+//Ability to name pet
+
+//CSS for general page styling
+
+//Increase pet's age every x minutes
+
+//Increase hunger, sleepiness, and boredom at chosen increment
+
+//Pet dies if any of them hits 10
+
+//Change image of pet at certain ages
+
+//Animate pet while it's alive
