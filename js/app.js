bb3924e241618b2b5b8ff3f6e3dbd332e28ccdc5
LehmanBrother <chrislehman1991@gmail.com>
2018-09-25 15:43:12 -0500

    added more button and interval logic

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ class Tamagotchi {
 
 const game = {
 	totalIntervals: 0,
+	light: true,
 	startGame() {
 		this.displayHunger();
 		this.displaySleepiness();
@@ -40,33 +41,75 @@ const game = {
 
 const jerry = new Tamagotchi("Jerry Smith",1,1,1,0);
 
+$('button.hunger').on('click', () => {
+	if(jerry.hunger >= 6) {
+		jerry.hunger-=5;
+	} else {
+		jerry.hunger = 1;
+	}
+	game.displayHunger();
+})
+
+$('button.sleepiness').on('click', () => {
+	if(game.light === true) {
+		$('body').css('background-color','black');
+		$('h1').css('color','white');
+		game.light = false;
+	} else {
+		$('body').css('background-color','white');
+		$('h1').css('color','black');
+		game.light = true;
+	}
+})
+
+$('button.boredom').on('click', () => {
+	if(jerry.boredom >= 6) {
+		jerry.boredom-=5;
+	} else {
+		jerry.boredom = 1;
+	}
+	game.displayBoredom();
+})
+
 setInterval(() => {
 	game.totalIntervals++;
-	//increase boredom every 1
-	//increase hunger every 2
-	//increase sleepiness every 3
-	jerry.boredom++;
-	game.displayBoredom();
-	if(game.totalIntervals % 2 === 0) {
-		jerry.hunger++;
-		game.displayHunger();
-	}
-	if(game.totalIntervals % 3 === 0) {
-		jerry.sleepiness++;
-		game.displaySleepiness();
+	if(game.light === true) {
+		jerry.boredom++;
+		game.displayBoredom();
+		if(game.totalIntervals % 2 === 0) {
+			jerry.hunger++;
+			game.displayHunger();
+		}
+		if(game.totalIntervals % 3 === 0) {
+			jerry.sleepiness++;
+			game.displaySleepiness();
+		}
+		if(game.totalIntervals % 10 === 0) {
+			jerry.age++;
+			game.displayAge();
+		}
+	} else {
+		if(game.totalIntervals % 4 === 0) {
+			jerry.hunger++;
+			game.displayHunger();
+		}
+		if(game.totalIntervals % 3 === 0) {
+			if(jerry.sleepiness >= 3) {
+				jerry.sleepiness-=2;
+			} else {
+				jerry.sleepiness = 1;
+			}
+			game.displaySleepiness();
+		}
+		if(game.totalIntervals % 10 === 0) {
+			jerry.age++;
+			game.displayAge();
+		}
 	}
 },
 1000)
 
 game.startGame();
-
-//Display image to represent pet
-
-//Display metrics:
-	//Hunger (1-10)
-	//Sleepiness (1-10)
-	//Boredom (1-10)
-	//Age
 
 //Buttons to feed, turn off lights, play
 
